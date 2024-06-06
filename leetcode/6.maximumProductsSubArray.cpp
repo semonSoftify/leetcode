73c9dca613a00d4845599f7b905b564f3b97f7df
semonSoftify <mohiminul.softifybd@gmail.com>
2024-06-06 15:33:31 +0600

    Add files via upload

---
+++ leetcode/6.maximumProductsSubArray.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        if (nums.empty())
+            return 0;
+
+        int max_product = nums[0];
+        int min_product = nums[0];
+        int result = nums[0];
+
+        for (size_t i = 1; i < nums.size(); ++i) {
+            if (nums[i] < 0) {
+                swap(
+                    max_product,
+                    min_product);
+            }
+
+            max_product = max(nums[i], max_product * nums[i]);
+            min_product = min(nums[i], min_product * nums[i]);
+
+            result = max(result, max_product);
+        }
+
+        return result;
+    }
+};
